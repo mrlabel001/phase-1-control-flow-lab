bb219280e7c25f5e488421b3e28b7314c9dc2ba4
mrlabel <farbean.makini@student.moringaschool.com>
2023-09-20 07:44:13 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(disInFeet) {
 
-function ternaryCheckCity(){
-  // Write your code here!
+  if (disInFeet <= 400){
+          return "This one is on me!";
+  }
+  else if (disInFeet > 2000 && disInFeet < 2500){
+      return "I will gladly take your thirty bucks.";
+  }
+  else if(disInFeet > 2500){
+      return "No can do.";
+  }
 }
+function ternaryCheckCity(city) {
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  if (city === "NYC") {
+      return "Ok, sounds good.";
+  }
+  else{
+      return "No go.";
+  }
+}
+function switchOnCharmFromTip(tip) {
+
+  if (tip === "generous") {
+      return "Thank you so much.";
+  } else if (tip === "not as generous") {
+      return "Thank you.";
+  } else {
+      return "Bye.";
+  }
+
+
+
+
 }
